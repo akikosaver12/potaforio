dc335085ee3c05ae56db05c85c03f2cc36e5c6fd
Manuel <manolosanchez2005@gmail.com>
2025-05-09 08:43:13 -0500

    pate2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import tailwindcss from './assets/tailwindcss.jpg'
 import viteLogo from '/vite.svg'
 import  persona from './assets/persona.jpg'
+import html from './assets/html.jpg'
+import css from './assets/css.jpg'
+import js from './assets/js.jpg'
+import react from './assets/react.jpg'
+import git  from './assets/git.jpg'
 import './App.css'
 
 function App() {
@@ -15,58 +20,100 @@ function App() {
           
   <div class="flex flex-col-reverse  w-[80%] ">
   <div class="flex flex-row   ">
-  <div class="iamgen h-[32rem]  w-[32rem] "  >
-    <img src={persona} alt="" />
+  <div class="iamgen   w-[24rem] "  >
+    <img className='float-right' src={persona} alt="" />
   </div>
 
 </div>
 
 
 
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[100%]">01</div>
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[100%]">02</div>
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[100%]">03</div>
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[100%]">conocimientos
-
-  <br/>
-  
-  </div>
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[600px]">05</div>
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[600px]">06</div>
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[600px]">07</div>
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[600px]">Apellido
-  <br/>
-  <p>Snchez Gomez </p>
-
-  </div>
-
-
-  <div class="size-16 bg-violet-500 mt-6 mb-8 w-[600px]">Nombre
-  <br/>
-  <p>Manuel Sanchez </p>
-  </div>
 
 
   
+
+  <div class="size-16  mt-6 mb-8 w-[100%] h-auto  text-2xl text-center  ">conocimientos 
+
+  <br/>
+
+  <div class="grid grid-cols-4 gap-4">
+  <div><p class="text-2xl text-center  ">html</p>
+  
+  <img class="rounded-[9vw] w-[80%] "  src={html} alt="" />
+  
+  </div>
+  <div><p class="text-2xl text-center ">css</p>
+  
+  <img class="rounded-[9vw] w-[70%] " src={css} alt="" />
+  
+  </div>
+  <div><p class="text-2xl text-center ">js</p>
+  
+  <img class="rounded-[9vw] w-[80%] " src={js} alt="" />
+  
+  </div>
+  <div><p class="text-2xl text-center ">React</p>
+  
+  <img class="rounded-[9vw]  w-[80%]" src={react} alt="" />
+  
+  </div>
+  <div><p class="text-2xl text-center ">tailwindcss</p>
+  
+  <img class="rounded-[9vw] w-[80%] " src={tailwindcss} alt="" />
+
+  </div>
+
+
+  <div><p class="text-2xl text-center ">Git</p>
+  <a href="https://github.com/dashboard">
+
+  <img class="rounded-[9vw] w-[80%] " src={git} alt="" />
+  </a>
+  
+
+  </div>
+</div>
+
+  </div>
+
+
+  
+
+
+  <div class="size-16  mt-6 mb-8 w-[600px] text-2xl ">apedisaje
+  <br />
+  <p class="text-xl text-center">grado 11 ,tecnicgo en sistema</p>
+
+
+  </div>
+
+
+  <div class="size-16  mt-6 mb-8 w-[600px] text-2xl ">edad
+
+  <br />
+  <p class="text-xl text-center">19 años</p>
+  </div>
+  
+
+
+  <div class="size-16  mt-6 mb-8 w-[600px] text-2xl ">Apellido
+  <br/>
+  <p class="text-xl text-center">Snchez Gomez </p>
+
+  </div>
+
+
+  <div class="size-16  mt-6 mb-8 w-[600px] text-2xl ">Nombre
+  <br/>
+  <p class="text-xl text-center">Manuel Sanchez </p>
+  </div>
+
+
+  
 </div>
 </div>
 </div>
+
 
       
     </>
